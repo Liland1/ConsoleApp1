949fa84ca8a06237d338f850085f04fba07d5e15
Liland1 <dimasicananasic80lll80@gmail.com>
2025-02-01 18:09:08 +0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+
+int[] conc = [];
+int sum = 0;
+for (int i = 0; i < 101; i++)
+{
+    Console.WriteLine("ВВедите число №" + i);
+    conc[i] = int.Parse(Console.ReadLine());
+    if ((conc[i] % 10 == 3) & (conc[i] % 7 == 0))
+    {
+        sum = sum + conc[i];
+    }
+    if (conc[i] == 0)
+    {
+        break;
+    }
+
+
+
+}
+Console.WriteLine(sum);
